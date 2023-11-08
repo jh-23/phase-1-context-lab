98c0ff0d27454778060eba101a04a5f0d2023230
John Heger <heger7@gmail.com>
2023-11-08 16:48:37 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
 /* Your Code Here */
 
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    const Object = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return Object 
+}
+
+function createEmployeeRecords (nestedArray) {
+    const employeeRecords = nestedArray.map(arrayNew => createEmployeeRecord(arrayNew))
+    return employeeRecords
+}
+
+function createTimeInEvent(dateStamp) {
+    console.log(this)
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateStamp.slice(11, 15)),
+        date: dateStamp.slice(0, 10)
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateStamp.slice(11, 15)),
+        date: dateStamp.slice(0, 10)
+    })
+    return this 
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find((element) => element.date === date).hour
+    const timeOut = this.timeOutEvents.find((element) => element.date === date).hour
+    return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(date) {
+    const rate = this.payPerHour
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    return rate * hoursWorked
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find((record) => record.firstName === firstName)
+}
+
+function calculatePayroll(arrayRecord) {
+    const sumEmployeePay = arrayRecord.map((employee) => allWagesFor.call(employee))
+    return sumEmployeePay.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
